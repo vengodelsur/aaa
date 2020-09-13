2232c098c32133e47ebaf852c83f2d81e9c18708
ekaterina <ekatlmsh@yandex.ru>
2020-09-13 19:10:18 +0300

    switch to id storing

--- oh_my_duck/main.py
+++ oh_my_duck/main.py
@@ -4,26 +4,27 @@ from typeguard import typechecked
 
 
 @typechecked
-class DialogNode:
-    def __init__(self, header_message: str):
-        self.header_message = header_message
-        self.transition_messages = []  # List[str]
-        self.transition_nodes = []  # List['DialogNode']
-
-    def add_transition(self, message: str, node: 'DialogNode'):
-        self.transition_messages.append(message)
-        self.transition_nodes.append(node)
-
-    def get_choice_message(self) -> str:
-        options = "\n".join(f"{i}) {message}" for i, message in enumerate(self.transition_messages))
-        return f"{self.header_message}\n{options}"
-
-    def get_transition_node(self, i: int):
-        return self.transition_nodes[i]  # TODO: error handling
-
-
-@typechecked
 class DialogGraph:
+    @typechecked
+    class Node:
+        def __init__(self, node_id: str, header_message: str):
+            self.node_id = node_id  # str
+            self.header_message = header_message  # str
+            self.transition_messages = []  # List[str]
+            self.transition_nodes = []  # List[str]
+
+        def add_transition(self, message: str, node_id: str):
+            self.transition_messages.append(message)
+            self.transition_nodes.append(node_id)
+
+        def get_choice_message(self) -> str:
+            options = "\n".join(
+                f"{i}) {message}" for i, message in enumerate(self.transition_messages))
+            return f"{self.header_message}\n{options}"
+
+        def get_transition_node_id(self, i: int) -> str:
+            return self.transition_nodes[i]  # TODO: error handling
+
     def __init__(self):
         self.nodes_dictionary = {}  # Dict[str, 'DialogNode']
 
@@ -34,7 +35,8 @@ class DialogGraph:
             json_dialog_info = json.load(f)
         # read nodes
         for node_info in json_dialog_info:
-            node = DialogNode(node_info["header_message"])
+            node = cls.Node(node_id=node_info["node_id"],
+                            header_message=node_info["header_message"])
             # TODO: check if node exists
             # TODO: function for node adding
             graph.nodes_dictionary[node_info["node_id"]] = node
@@ -43,8 +45,7 @@ class DialogGraph:
             # TODO: node getting
             node = graph.nodes_dictionary[node_info["node_id"]]
             for transition in node_info["transitions"]:
-                target_node = graph.nodes_dictionary[transition["target_node_id"]]
-                node.add_transition(transition["message"], target_node)
+                node.add_transition(transition["message"], transition["target_node_id"])
         return graph
 
     def to_img(self):
@@ -57,4 +58,5 @@ class DialogGraph:
 if __name__ == '__main__':
     graph = DialogGraph.from_json('scenario.json')
     # TODO: may be switch to storing transitions in graph in terms of ids?
+    # TODO: get rig of the dictionary?
     # print([(node_id, node.transition_nodes) for node_id, node in graph.nodes_dictionary.items()])
