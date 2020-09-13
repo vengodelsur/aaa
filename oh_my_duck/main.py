b783e723b1cbf2b0a0ca518a619cda27b42f647e
ekaterina <ekatlmsh@yandex.ru>
2020-09-13 19:29:38 +0300

    add dialog manager

--- oh_my_duck/main.py
+++ oh_my_duck/main.py
@@ -25,12 +25,16 @@ class DialogGraph:
         def get_transition_node_id(self, i: int) -> str:
             return self.transition_nodes[i]  # TODO: error handling
 
-    def __init__(self):
+        def __bool__(self):
+            return bool(self.transition_nodes)
+
+    def __init__(self, root_node_id: str = 'start'):
         self.nodes_dictionary = {}  # Dict[str, 'DialogNode']
+        self.root_node_id = root_node_id
 
     @classmethod
-    def from_json(cls, filepath: str) -> 'DialogGraph':
-        graph = cls()
+    def from_json(cls, filepath: str, root_node_id: str = 'start') -> 'DialogGraph':
+        graph = cls(root_node_id)
         with open(filepath) as f:
             json_dialog_info = json.load(f)
         # read nodes
@@ -48,8 +52,31 @@ class DialogGraph:
                 node.add_transition(transition["message"], transition["target_node_id"])
         return graph
 
+    def get_node(self, node_id: str) -> Node:
+        return self.nodes_dictionary[node_id]
+
     def to_img(self):
         pass
+
+
+
+
+class DialogManager:
+    def __init__(self, graph: DialogGraph):
+        self.graph = graph
+        self.current_node_id = graph.root_node_id
+
+    def read_user_input(self):
+        current_node = self.graph.get_node(self.current_node_id)
+        print(current_node.get_choice_message())
+        # TODO: handling incorrect input
+        chosen_option = int(input())
+        self.current_node_id = current_node.get_transition_node_id(chosen_option)
+
+    def run(self):
+        while self.graph.get_node(self.current_node_id):
+            self.read_user_input()
+        print("GAME OVER")
 
 
 # TODO: read user input
@@ -60,3 +87,5 @@ if __name__ == '__main__':
     # TODO: may be switch to storing transitions in graph in terms of ids?
     # TODO: get rig of the dictionary?
     # print([(node_id, node.transition_nodes) for node_id, node in graph.nodes_dictionary.items()])
+    manager = DialogManager(graph)
+    manager.run()
