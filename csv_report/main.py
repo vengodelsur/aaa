b2b7cc822821924d5a373b15e5456fb535541aeb
ekaterina <ekatlmsh@yandex.ru>
2020-09-27 21:31:25 +0300

    add docstrings

--- csv_report/main.py
+++ csv_report/main.py
@@ -3,7 +3,11 @@ import sys
 from typing import Dict, Optional, TextIO, Tuple
 
 
-def print_departments(csv_reader):
+def print_departments(csv_reader: csv.DictReader):
+    """
+    Печатает список отделов, встретившихся в csv
+    :param csv_reader: объект для чтени информации об отделах из csv
+    """
     departments = set()
     for row in csv_reader:
         # KeyError
@@ -14,6 +18,14 @@ def print_departments(csv_reader):
 
 
 def create_summary_report(csv_reader: csv.DictReader) -> Dict[str, Dict[str, int]]:
+    """
+    Создаёт отчёт о минимальной, максимальной, средней зарплате по отделам :param csv_reader:
+    объект для чтени информации об отделах из csv
+    :return: словарь со сводной информацией о зарплате по отделам, например:
+        {'Продажи': {'department_name': 'Продажи', 'min_salary': 100500, 'max_salary': 100500,
+                     'mean_salary': 100500.0,'staff_number': 1, 'salary_sum': 100500}}
+    """
+
     departments_stats = dict()
     for row in csv_reader:
         department_name = row['Отдел']
@@ -39,7 +51,14 @@ def create_summary_report(csv_reader: csv.DictReader) -> Dict[str, Dict[str, int
 
 
 def print_summary_report(report: Dict[str, Dict[str, int]], output_filename: Optional[str] = None):
-    print(report)
+    """
+    Выводит/сохраняет отчёт о минимальной, максимальной, средней зарплате по отделам
+    :param report: словарь с информацией по отделам, например:
+        {'Продажи': {'department_name': 'Продажи', 'min_salary': 100500, 'max_salary': 100500,
+                     'mean_salary': 100500.0}}
+    :param output_filename: имя файла для вывода отчета (если None, используется стандартный вывод)
+    :return:
+    """
     keys = ('department_name', 'min_salary', 'max_salary', 'mean_salary')
     if output_filename is None:
         report_to_csv(report, keys, sys.stdout)
@@ -50,6 +69,16 @@ def print_summary_report(report: Dict[str, Dict[str, int]], output_filename: Opt
 
 def report_to_csv(report: Dict[str, Dict[str, int]], keys: Tuple[str, ...], f: TextIO,
                   delimiter: Optional[str] = '\t'):
+    """
+    Форматирует в csv и записывает отчёт
+    :param report: словарь с информацией по отделам, например:
+        {'Продажи': {'department_name': 'Продажи', 'min_salary': 100500, 'max_salary': 100500,
+                     'mean_salary': 100500.0}}
+    :param keys: ключи, которые нужно использовать из словаря с информацией, например:
+        ('department_name', 'min_salary', 'max_salary', 'mean_salary')
+    :param f: TextIO объект - куда записывать csv
+    :param delimiter: разделитель
+    """
     dict_writer = csv.DictWriter(f, keys, delimiter=delimiter)
     dict_writer.writeheader()
     for department_stats in report.values():
@@ -57,6 +86,11 @@ def report_to_csv(report: Dict[str, Dict[str, int]], keys: Tuple[str, ...], f: T
 
 
 def choose_option(options_number: int) -> int:
+    """
+    Запрашивает у пользователя порядковый номер пункта меню (с проверкой корректности ввода)
+    :param options_number: количество пунктов в меню
+    :return: номер выбранного пользователем пункта меню
+    """
     while True:
         try:
             option = int(input())
@@ -67,6 +101,12 @@ def choose_option(options_number: int) -> int:
 
 
 def main():
+    """
+    По выбору пользователя выводит информацию из v отчёта о сотрудниках компании:
+    выводит список отделов
+    выводит сводный отчёт (минимальная, максимальная, средняя зарплата по отделам)
+    сохраняет сводный отчёт в файл (минимальная, максимальная, средняя зарплата по отделам)
+    """
     input_filename = 'funcs_homework_employees_sample.csv'
     output_filename = 'report.csv'
     delimiter = ';'
